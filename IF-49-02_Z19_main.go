2d1cf8eebd708110d6b45274678e359a0d09b4e3
Pidiota <khalidahmadnaufal26@gmail.com>
2026-05-28 22:54:32 +0700

    tambahan function cariResep dan Algoritma pilih menu

--- IF-49-02_Z19_main.go
+++ IF-49-02_Z19_main.go
@@ -1,52 +1,76 @@
 package main
 import "fmt"
 
+type resep struct{
+	nama string
+	bahan string
+	langkah string
+	durasi int
+
+}
+
+type tabResep [100]resep
+
 func main() {
-	var x int
-	fmt.Println("----- selamat datang di ResepKu -----")
+	var x, opsi int
+	var target string
+	var T tabResep
+	x = 99
+	for x != 0 {
+		fmt.Println("----- selamat datang di ResepKu -----")
+		fmt.Println("Silahkan pilih opsi di bawah ini:")
+		fmt.Println("1 - Cari resep")
+		fmt.Println("2 - Tambah resep")
+		fmt.Println("3 - Ubah resep")
+		fmt.Println("4 - Hapus resep")
+		fmt.Println("99 - Keluar")
+		opsi = 0
+
+		for opsi != 99 {
+			fmt.Scan(&opsi)
+			if opsi == 1 {
+				cariResep(T, target)
+			} else if opsi == 2 {
+
+			} else if opsi == 3 {
+
+			} else if opsi == 4 {
+
+			} else{
+				fmt.Println("Input Tidak Valid")
+				opsi = 0
+			}
+		}
+		
+
+	}
+	
 	
 }
 
-func pilihMenu(opsi *int) {
-	fmt.Println("Silahkan pilih opsi di bawah ini:")
-	fmt.Println("1 - Cari resep")
-	fmt.Println("2 - Tambah resep")
-	fmt.Println("3 - Ubah resep")
-	fmt.Println("4 - Hapus resep")
-	fmt.Println("0 - Keluar")
-	fmt.Scan(&opsi)
-	if opsi == 1 {
 
-	} else if opsi == 2 {
+func cariResep(T tabResep, target string) {
+	fmt.Println("Silahkan Masukkan Nama Menu Yang Anda Cari")
+	fmt.Scan(&target)
 
-	} else if opsi == 3 {
-
-	} else if opsi == 4 {
-
-	} else{
-
-	}
-}
-
-func cariResep() {
-	for i := 0; i < n; i++{
-		if x[i].nilai == target {
-			fmt.Println(x[i].matkul, x[i].nilai)
+	for i := 0; i < n; i++{ // Bagian looping untuk scannya bingung kondisinya gmana
+		if T[i].nama == target {
+			fmt.Println(T[i].nama, T[i].bahan, T[i].langkah, T[i].durasi )
 		}
 	}
-	fmt.Println("Data Ditemukan!")
+	fmt.Println("Entahlah Isi Apa")
 }
 
 
-func tambahMenu(){
+//func tambahMenu(){
 
-}
+//}
 
-func ubahMenu() {
+//func ubahMenu() {
 
-}
+//}
 
-func hapusMenu() {
+//func hapusMenu() {
 
-}
+//}
 
