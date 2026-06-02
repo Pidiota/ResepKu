f4209cd8b0d3cd8cab439103476a952c4409ea37
Pidiota <khalidahmadnaufal26@gmail.com>
2026-06-02 18:13:17 +0700

    sedikit yapp

--- IF-49-02_Z19_main.go
+++ IF-49-02_Z19_main.go
@@ -1,6 +1,7 @@
 package main
 import "fmt"
 
+const NMAX int = 100
 type resep struct{
 	nama string
 	bahan string
@@ -9,7 +10,7 @@ type resep struct{
 
 }
 
-type tabResep [100]resep
+type tabResep [NMAX]resep
 
 func main() {
 	var x, opsi int
@@ -23,26 +24,56 @@ func main() {
 		fmt.Println("2 - Tambah resep")
 		fmt.Println("3 - Ubah resep")
 		fmt.Println("4 - Hapus resep")
-		fmt.Println("99 - Keluar")
-		opsi = 99
-
+		fmt.Println("0 - Keluar")
+		fmt.Scan(&opsi)
 		for opsi != 0 {
-			fmt.Scan(&opsi)
 			if opsi == 1 {
 				cariResep(T, target)
+				fmt.Println("Silahkan pilih opsi di bawah ini:")
+				fmt.Println("1 - Cari resep")
+				fmt.Println("2 - Tambah resep")
+				fmt.Println("3 - Ubah resep")
+				fmt.Println("4 - Hapus resep")
+				fmt.Println("0 - Keluar")
+				fmt.Scan(&opsi)
 			} else if opsi == 2 {
-
+				tambahMenu(&T)
+				fmt.Println("Silahkan pilih opsi di bawah ini:")
+				fmt.Println("1 - Cari resep")
+				fmt.Println("2 - Tambah resep")
+				fmt.Println("3 - Ubah resep")
+				fmt.Println("4 - Hapus resep")
+				fmt.Println("0 - Keluar")
+				fmt.Scan(&opsi)
 			} else if opsi == 3 {
-
+				//
+				fmt.Println("Silahkan pilih opsi di bawah ini:")
+				fmt.Println("1 - Cari resep")
+				fmt.Println("2 - Tambah resep")
+				fmt.Println("3 - Ubah resep")
+				fmt.Println("4 - Hapus resep")
+				fmt.Println("0 - Keluar")
+				fmt.Scan(&opsi)
 			} else if opsi == 4 {
-
-			} else{
-				fmt.Println("Input Tidak Valid")
-				opsi = 0
+				//
+				fmt.Println("Silahkan pilih opsi di bawah ini:")
+				fmt.Println("1 - Cari resep")
+				fmt.Println("2 - Tambah resep")
+				fmt.Println("3 - Ubah resep")
+				fmt.Println("4 - Hapus resep")
+				fmt.Println("0 - Keluar")
+				fmt.Scan(&opsi)
+			} else {
+				fmt.Println("input tidak valid")
+				fmt.Println("Silahkan pilih opsi di bawah ini:")
+				fmt.Println("1 - Cari resep")
+				fmt.Println("2 - Tambah resep")
+				fmt.Println("3 - Ubah resep")
+				fmt.Println("4 - Hapus resep")
+				fmt.Println("0 - Keluar")
+				fmt.Scan(&opsi)
 			}
 		}
-		
-
 	}
 	
 	
@@ -51,7 +82,7 @@ func main() {
 func sortMenu(T tabResep) {
 	var pass, k, n int
 	var temp resep
-	n = 99
+	n ;= 100
 	for pass = 1; pass < n; pass++{
 		k = pass
 		temp = T[k]
@@ -59,7 +90,7 @@ func sortMenu(T tabResep) {
 			T[k] = T[k-1]
 			k = k-1
 		}
-	T[k] = temp	
+		T[k] = temp	
 	}
 }
 
@@ -67,8 +98,7 @@ func sortMenu(T tabResep) {
 func cariResep(T tabResep, target string) {
 	fmt.Println("Silahkan Masukkan Nama Menu Yang Anda Cari")
 	fmt.Scan(&target)
-	n := 99
-
+	n := 100
 	for i := 0; i < n; i++{
 		if T[i].nama == target {
 			fmt.Println(T[i].nama, T[i].bahan, T[i].langkah, T[i].durasi )
@@ -78,9 +108,19 @@ func cariResep(T tabResep, target string) {
 }
 
 
-//func tambahMenu(T tabResep){
-
-//}
+func tambahMenu(T *tabResep){
+	var n, i int	
+	for i = 5 ; i < n; i++{
+		fmt.Print("Nama Menu :")
+		fmt.Scan(&T[i].nama)
+		fmt.Print("Detail Bahan :")
+		fmt.Scan(&T[i].bahan)
+		fmt.Print("Langkah - langkah :")
+		fmt.Scan(&T[i].langkah)
+		fmt.Print("Durasi")
+		fmt.Scan(&T[i].durasi)
+	} 
+}
 
 //func ubahMenu() {
 
@@ -89,4 +129,5 @@ func cariResep(T tabResep, target string) {
 //func hapusMenu() {
 
 //}
+
 
